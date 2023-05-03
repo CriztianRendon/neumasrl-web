7181def36227f82715b64ef0c5f26dfa0b94cfbf
CrisRendon <crizthianrendon@gmail.com>
2023-05-03 11:45:12 -0300

    staff medico actualizado

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,6 @@ import { useTranslation } from 'react-i18next';
 import CardsEspecialidades from '../components/CardsEspecialidades';
 import Equipo from '../components/Equipo';
 import Hero from '../components/Hero';
-// import Noticias from '../components/Noticias';
 import CardsCoberturas from '../components/CardsCoberturas';
 
 const Home = () => {
@@ -14,7 +13,7 @@ const Home = () => {
 			<Hero />
 			<Equipo />
 			<CardsEspecialidades />
-			{lang === 'es' ? <CardsCoberturas /> : ''}
+			{lang === 'es' && <CardsCoberturas />}
 		</>
 	);
 };
