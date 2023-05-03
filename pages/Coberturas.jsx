59ddabb0b48f58642fac8aa0669f2ce77e4370a4
CrisRendon <crizthianrendon@gmail.com>
2023-05-03 10:31:04 -0300

    fix reload

--- pages/Coberturas.jsx
+++ pages/Coberturas.jsx
@@ -1,11 +1,13 @@
+import { Navigate } from 'react-router-dom';
+
 import CardsCoberturas from '../components/CardsCoberturas';
+import { useTranslation } from 'react-i18next';
 
 const Coberturas = () => {
-	return (
-		<>
-			<CardsCoberturas />
-		</>
-	);
+	const [t, i18n] = useTranslation('global');
+	const lang = i18n.language;
+
+	return <>{lang === 'es' ? <CardsCoberturas /> : <Navigate to='/' />}</>;
 };
 
 export default Coberturas;
